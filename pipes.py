fd608aaa5bcb7c3ec9a5281d9fd07eaa74a5623f
Prorok <prorok@ubuntu.ubuntu-domain>
2010-12-06 06:49:47 +0800

    added pipes frame, and nicr cstest

---
+++ pipes.py
@@ -0,0 +1,26 @@
+def write_back_unit(postmem, postalu, isempty ):
+
+def instruction_fetch():
+
+
+def pre_issue_buffer():
+
+
+def issue():
+
+def pre_mem_buffer():
+
+
+def pre_alu_buffer():
+
+
+def MEM():
+
+
+def ALU():
+
+
+def post_mem_buffer():
+
+
+def post_alu_buffer():
