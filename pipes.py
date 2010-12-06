b715353023fb22f53f6af06685466456b15f5df0
verch <verch101@verch.local>
2010-12-05 18:46:45 -0600

    more documentation expanded

--- pipes.py
+++ pipes.py
@@ -7,6 +7,7 @@ cyclenum = 0
 #all of the out pust stuff is here I dont know why.
 print '--------------------'
 print 'Cycle:', cyclenum
+print ' '
 print 'Pre-Issue Buffer:'
 print '        Entry 0:', preissuebuffer0
 print '        Entry 1:', preissuebuffer1
@@ -24,6 +25,18 @@ print '        Entry 1:', preMEMbuffer1
 print 'Post_MEM Queue:'
 print '        Entry 0:', postMEMbuffer0
 print '        Entry 1:', postMEMbuffer1
+print ' '
+print 'Registers'
+print 'R00:', '        ', registers[0],'        ', registers[1],'        ', registers[2],'        ', registers[3],'        ', registers[4],'        ', registers[5],'        ', registers[6],'        ', registers[7]
+print 'R08:', '        ', registers[8],'        ', registers[9],'        ', registers[10],'        ', registers[11],'        ', registers[12],'        ', registers[13],'        ', registers[14],'        ', registers[15]
+print 'R16:', '        ', registers[16],'        ', registers[17],'        ', registers[18],'        ', registers[19],'        ', registers[20],'        ', registers[21],'        ', registers[22],'        ', registers[23]
+print 'R24:', '        ', registers[24],'        ', registers[25],'        ', registers[26],'        ', registers[27],'        ', registers[28],'        ', registers[29],'        ', registers[30],'        ', registers[31]
+
+#Cash stuff goes here But I dont understand that stuff yet, so this is just a black hole for right now. We can just put something in here later to make the diff closer.
+
+print 'Data'
+print '128:	4	16
+print '--------------------'
 
 cyclenum = cyclenum + 1
 def instruction_fetch():
