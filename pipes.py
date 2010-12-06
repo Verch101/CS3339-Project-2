9e3f0f367be34f83e97ced33defb68a251fa3706
verch <verch101@verch.local>
2010-12-05 18:19:24 -0600

    first try to push back up

---
+++ pipes.py
@@ -0,0 +1,27 @@
+postmem, postalu = '0', '0'
+def write_back_unit(postmem, postalu, isempty ):
+cyclenum = 0
+
+print '--------------------'
+print 'Cycle:', cyclenum
+print 'Pre-Issue Buffer:'
+print '        Entry 0:', preissuebuffer0
+print '        Entry 1:', preissuebuffer1
+print '        Entry 2:', preissuebuffer2
+print '        Entry 3:', preissuebuffer3
+print 'Pre_ALU Queue:'
+print '        Entry 0:', preALUbuffer0
+print '        Entry 0:', preALUbuffer1
+
+cyclenum = cyclenum + 1
+def instruction_fetch():
+
+
+
+def issue():
+
+def MEM():
+
+
+def ALU():
+
